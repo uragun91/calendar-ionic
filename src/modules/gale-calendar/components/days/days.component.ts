f487e4315f5dd1bfb6138dd80af81a84f2b600f6
uragun91 <f.vanya91@gmail.com>
2023-07-11 18:28:49 +0200

    1 file modified

--- src/modules/gale-calendar/components/days/days.component.ts
+++ src/modules/gale-calendar/components/days/days.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { getDate } from 'date-fns';
 
 @Component({
   selector: 'gale-days',
   templateUrl: './days.component.html',
   styleUrls: ['./days.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DaysComponent implements OnInit {
   @Input() days!: Date[];
