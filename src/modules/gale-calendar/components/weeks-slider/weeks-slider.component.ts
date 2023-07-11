d5afd8e35da3969c79ed0158373624b0d218ec98
uragun91 <f.vanya91@gmail.com>
2023-07-11 11:32:47 +0200

    added weeks slider

---
+++ src/modules/gale-calendar/components/weeks-slider/weeks-slider.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weeks-slider',
+  templateUrl: './weeks-slider.component.html',
+  styleUrls: ['./weeks-slider.component.css'],
+})
+export class WeeksSliderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
